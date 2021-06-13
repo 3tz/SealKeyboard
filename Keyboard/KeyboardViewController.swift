fc4ef2cb08ae86f96a8dd6e06d7c517739e8d177
Timothy Zhang <tsme4wk@gmail.com>
2021-06-13 16:42:50 -0400

    add keyboard list while holding globe & request clears text field

--- Keyboard/KeyboardViewController.swift
+++ Keyboard/KeyboardViewController.swift
@@ -27,7 +27,8 @@ class KeyboardViewController: UIInputViewController {
     ).instantiate(withOwner: self, options: nil)[0] as? UIView
     
     // Actually make the globe button switch keyboard
-    nextKeyboardButton.addTarget(self, action: #selector(advanceToNextInputMode), for: .touchUpInside)
+    nextKeyboardButton.addTarget(
+      self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
     decryptedMsg.text = ""
   }
   
@@ -55,15 +56,40 @@ class KeyboardViewController: UIInputViewController {
 
   @IBAction func requestButtonPressed(_ sender: Any) {
     // TODO: finish
+    var msg: String!
+    msg = "req_aes|my_public_key"
+    
+    clearInputText()
+    
+    textDocumentProxy.insertText(msg)
     decryptedMsg.text = "request pressed"
   }
+  
   @IBAction func decryptButtonPressed(_ sender: Any) {
     // TODO: finish
     decryptedMsg.text = "decrypt pressed"
   }
+  
   @IBAction func encryptButtonPressed(_ sender: Any) {
     // TODO: finish
     decryptedMsg.text = "encrypt pressed"
   }
   
+  
+  /// Clear the input text field if it's not empty.
+  func clearInputText() {
+    if textDocumentProxy.hasText {
+      let textBeforeInput = textDocumentProxy.documentContextBeforeInput ?? ""
+      let textAfterInput = textDocumentProxy.documentContextAfterInput ?? ""
+      let selectedText = textDocumentProxy.selectedText ?? ""
+      
+      // move cursor to the end of the text input
+      textDocumentProxy.adjustTextPosition(byCharacterOffset: textAfterInput.count)
+      
+      // delete backward n times where n is the length of the text
+      for _ in 0..<textAfterInput.count + textBeforeInput.count + selectedText.count {
+        textDocumentProxy.deleteBackward()
+      }
+    }
+  }
 }
