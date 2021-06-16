ee1bccc4e7b24f4c369e9bcb8b41821f9383fa33
Timothy Zhang <tsme4wk@gmail.com>
2021-06-16 02:29:16 -0400

    inputs and outputs of Keys's methods are now strings

--- Seal/Keys.swift
+++ Seal/Keys.swift
@@ -28,22 +28,27 @@ final class Keys {
     symmetricKey = SymmetricKey(size: .bits256)
   }
 
+  // MARK: - Asymmetric key exchange (symmetric key generation) methods
+
   /// Generates an ephemeral key agreement key and performs key agreement to  derive the symmetric encryption key.
   /// Modified from: developer.apple.com/documentation/cryptokit/performing_common_cryptographic_operations
   /// - Parameters:
-  ///   - theirEncryptionPublicKey: Encryption public key of the recipient.
+  ///   - theirEncryptionPublicKeyString: Encryption public key of the recipient.
   /// - Throws: TODO:
   /// - Returns:
   ///   - Public key of the ephemeral secret key used for generating the symmetric key.
   ///   - Signature signed by signing secret key.
   ///   - Public key of the signing secret key.
-  func ECDHKeyExchange(
-    with theirEncryptionPublicKey: Curve25519.KeyAgreement.PublicKey
-  ) throws ->(
-    ephemeralPublicKeyData: Data,
-    signature: Data,
-    signingPublicKey: Curve25519.Signing.PublicKey
+  func ECDHKeyExchange(with theirEncryptionPublicKeyString: String) throws ->(
+    ephemeralPublicKeyString: String,
+    signatureString: String,
+    signingPublicKeyString: String
   ) {
+    // Convert string back to public key type
+    let theirEncryptionPublicKey = try Curve25519.KeyAgreement.PublicKey(
+      rawRepresentation: asData(theirEncryptionPublicKeyString)
+    )
+
     let ephemeralSecretKey = Curve25519.KeyAgreement.PrivateKey()
     let ephemeralPublicKey = ephemeralSecretKey.publicKey
 
@@ -62,28 +67,45 @@ final class Keys {
       for: ephemeralPublicKey.rawRepresentation + theirEncryptionPublicKey.rawRepresentation
     )
 
-    return (ephemeralPublicKey.rawRepresentation, signature, signingPublicKey)
+    return (
+      asString(ephemeralPublicKey.rawRepresentation),
+      asString(signature),
+      asString(signingPublicKey.rawRepresentation)
+    )
   }
 
-  /// Generates an ephemeral key agreement key and the performs key agreement to get the shared secret and derive the
-  ///   symmetric encryption key.
-  /// From: developer.apple.com/documentation/cryptokit/performing_common_cryptographic_operations
+  /// Verifiy the response of the key exchange by checking the signature and finish the key exchange by computing the
+  ///   symmetric key based on the ephmeral public key sent by the sender.
+  /// Modified from: developer.apple.com/documentation/cryptokit/performing_common_cryptographic_operations
+  /// - Parameters:
+  ///   - ephemeralPublicKeyString: The public key of the key pair used for generating the symmetric key.
+  ///   - signatureString: Signature of the sender on the ephermeral public key + our encryption public key
+  ///   - theirSigningPublicKeyString: Signing public key of the sender
+  /// - Throws: Error if signature unmatch or incorrect key information TODO: finish
   func verifyECDHKeyExchangeResponse(
-      ephemeralPublicKeyData: Data,
-      signature: Data,
-      theirSigningPublicKey: Curve25519.Signing.PublicKey
+      _ ephemeralPublicKeyString: String,
+      _ signatureString: String,
+      _ theirSigningPublicKeyString: String
   ) throws {
+    // Convert input strings into corresponding data types
+    let ephemeralPublicKeyData = asData(ephemeralPublicKeyString)
+    let signature = asData(signatureString)
+    let theirSigningPublicKey = try Curve25519.Signing.PublicKey(
+      rawRepresentation: asData(theirSigningPublicKeyString)
+    )
+
     // Verify signature
     let data = ephemeralPublicKeyData + encryptionPublicKey.rawRepresentation
     guard theirSigningPublicKey.isValidSignature(signature, for: data) else {
       throw DecryptionErrors.authenticationError
     }
 
-
     let ephemeralPublicKey = try Curve25519.KeyAgreement.PublicKey(
       rawRepresentation: ephemeralPublicKeyData
     )
-    let sharedSecret = try encryptionSecretKey.sharedSecretFromKeyAgreement(with: ephemeralPublicKey)
+    let sharedSecret = try encryptionSecretKey.sharedSecretFromKeyAgreement(
+      with: ephemeralPublicKey
+    )
     symmetricKey = sharedSecret.hkdfDerivedSymmetricKey(
       using: SHA256.self,
       salt: protocolSalt,
@@ -94,37 +116,68 @@ final class Keys {
     )
   }
 
+  // MARK: - Symmetric encryption and decryption methods
+
   /// Encrypt via symmetric encryption with current symmetric key.
-  func encrypt(_ data: Data) throws -> (
-    ciphertext: Data,
-    signature: Data
+  /// - Parameter msg: Message to encrypt with current symmetric key
+  /// - Throws: TODO
+  /// - Returns:
+  ///   - ciphertextString: Ciphertext of the encrypted message.
+  ///   - signatureString: Signature signed with signing secret key.
+  ///   - signingPublicKeyString: Public key of the secret key used for signing the signature.
+  func encrypt(_ msg: String) throws -> (
+    ciphertextString: String,
+    signatureString: String,
+    signingPublicKeyString: String
   ){
+    let data = msg.data(using: .utf8)!
+
     let ciphertext = try ChaChaPoly.seal(data, using: symmetricKey).combined
     let signature = try signingSecretKey.signature(for: ciphertext)
 
-    return (ciphertext, signature)
+    return (
+      asString(ciphertext), asString(signature), asString(signingPublicKey.rawRepresentation)
+    )
   }
 
   /// Decrypt via symmetric encryption with current symmetric key.
+  /// - Parameters:
+  ///   - sealedMessage:
+  ///       - ciphertextString: The ciphertext received.
+  ///       - signatureString: Signature along the message.
+  ///   - theirSigningPublicKeyString: Public key of the secret key used for signing the signature.
+  /// - Throws: TODO
+  /// - Returns: Decrypted message.
   func decrypt(
-    _ sealedMessage: (ciphertext: Data, signature: Data),
-    from theirSigningPublicKey: Curve25519.Signing.PublicKey
-  ) throws -> Data {
+    _ sealedMessage: (ciphertextString: String, signatureString: String),
+    from theirSigningPublicKeyString: String
+  ) throws -> String {
 
-    let data = sealedMessage.ciphertext
+    let ciphertext = asData(sealedMessage.ciphertextString)
+    let signature = asData(sealedMessage.signatureString)
+    let theirSigningPublicKey = try Curve25519.Signing.PublicKey(
+      rawRepresentation: asData(theirSigningPublicKeyString)
+    )
 
-    guard theirSigningPublicKey.isValidSignature(sealedMessage.signature, for: data) else {
+    guard theirSigningPublicKey.isValidSignature(signature, for: ciphertext) else {
       throw DecryptionErrors.authenticationError
     }
 
+    let sealedBox = try! ChaChaPoly.SealedBox(combined: ciphertext)
+    let msg = try ChaChaPoly.open(sealedBox, using: symmetricKey)
 
-    let sealedBox = try! ChaChaPoly.SealedBox(combined: sealedMessage.ciphertext)
-
-    return try ChaChaPoly.open(sealedBox, using: symmetricKey)
+    return String(decoding: msg, as: UTF8.self)
   }
-
 }
 
 enum DecryptionErrors: Error {
     case authenticationError
+}
+
+func asData(_ str: String) -> Data {
+  return Data(base64Encoded: str)!
+}
+
+func asString(_ data: Data) -> String {
+  return data.withUnsafeBytes { Data(Array($0)).base64EncodedString()}
 }
