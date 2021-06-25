8bbb2bd4a29231680d68c2f9dd567f5c3601cb3a
Timothy Zhang <tsme4wk@gmail.com>
2021-06-25 18:26:17 -0400

    add actions to crypto buttons

--- Keyboard/Subviews/CryptoButtons.swift
+++ Keyboard/Subviews/CryptoButtons.swift
@@ -60,6 +60,11 @@ class CryptoBar {
     requestButton.setTitleColor(.white, for: [])
     requestButton.translatesAutoresizingMaskIntoConstraints = false
     requestButton.layer.cornerRadius = KeyboardSpecs.buttonCornerRadius
+    requestButton.addTarget(
+      self,
+      action: #selector(requestButtonPressed(_:)),
+      for: .touchUpInside
+    )
 
     let unsealButton = UIButton(type: .system)
     unsealButton.setTitle("Unseal Copied Text", for: .normal)
@@ -68,6 +73,11 @@ class CryptoBar {
     unsealButton.setTitleColor(.white, for: [])
     unsealButton.translatesAutoresizingMaskIntoConstraints = false
     unsealButton.layer.cornerRadius = KeyboardSpecs.buttonCornerRadius
+    unsealButton.addTarget(
+        self,
+        action: #selector(unsealButtonPressed(_:)),
+        for: .touchUpInside
+      )
 
     let sealButton = UIButton(type: .system)
     sealButton.setTitle("Seal Message Field Text", for: .normal)
@@ -76,141 +86,144 @@ class CryptoBar {
     sealButton.setTitleColor(.white, for: [])
     sealButton.translatesAutoresizingMaskIntoConstraints = false
     sealButton.layer.cornerRadius = KeyboardSpecs.buttonCornerRadius
+    sealButton.addTarget(
+      self,
+      action: #selector(sealButtonPressed(_:)),
+      for: .touchUpInside
+    )
 
     buttonsView = UIStackView(arrangedSubviews: [requestButton, unsealButton, sealButton])
     buttonsView.axis = .horizontal
     buttonsView.spacing = KeyboardSpecs.horizontalSpacing
     buttonsView.distribution = .fillProportionally
   }
-/*
+
   /// Request button pressed, so perform key exchange process by placing our encryption public key in the input text field.
-  func requestButtonPressed(_ sender: Any) {
-    textBox.text = "request pressed" // TODO: placeholder
+  @objc func requestButtonPressed(_ sender: Any) {
+    textView.text = "request pressed" // TODO: placeholder
     let msg = MessageType.ECDH0.rawValue + "|" +
       asString(keys.encryptionPublicKey.rawRepresentation)
-    clearInputText()
-    textDocumentProxy.insertText(msg)
+    controller.clearInputText()
+    controller.textDocumentProxy.insertText(msg)
   }
 
-func unsealButtonPressed(_ sender: Any) {
-  // TODO: finish
-  guard let copiedText = UIPasteboard.general.string else {
-    textBox.text = "No copied text found." // TODO: placeholder
-    return
-  }
-
-  let tokens = copiedText.components(separatedBy: "|")
-  NSLog("Tokens read: \(tokens)")
-
-  switch MessageType(rawValue: tokens[0]){
-  case .ECDH0:
-    // Request to initiate ECDH, i.e., to generate a symmetric key.
-    // Generate a symmetric key and send it over.
-    // Expected format: "{.ECDH0}|{sender's public key}"
-    if tokens.count != 2 { fallthrough }
-
-    let theirEncryptionPublicKeyString = tokens[1]
-
-    var ephemeralPublicKeyString, signatureString, signingPublicKeyString: String!
-    // Start ECDH, store the symmetric key, and send them the public key
-    do {
-      (ephemeralPublicKeyString, signatureString, signingPublicKeyString) =
-              try keys.ECDHKeyExchange(with: theirEncryptionPublicKeyString)
-    } catch {
-      NSLog(".ECDH0 error caught:\n\(error)")
-      fallthrough
+  @objc func unsealButtonPressed(_ sender: Any) {
+    // TODO: finish
+    guard let copiedText = UIPasteboard.general.string else {
+      textView.text = "No copied text found." // TODO: placeholder
+      return
     }
 
+    let tokens = copiedText.components(separatedBy: "|")
+    NSLog("Tokens read: \(tokens)")
+
+    switch MessageType(rawValue: tokens[0]){
+    case .ECDH0:
+      // Request to initiate ECDH, i.e., to generate a symmetric key.
+      // Generate a symmetric key and send it over.
+      // Expected format: "{.ECDH0}|{sender's public key}"
+      if tokens.count != 2 { fallthrough }
+
+      let theirEncryptionPublicKeyString = tokens[1]
+
+      var ephemeralPublicKeyString, signatureString, signingPublicKeyString: String!
+      // Start ECDH, store the symmetric key, and send them the public key
+      do {
+        (ephemeralPublicKeyString, signatureString, signingPublicKeyString) =
+                try keys.ECDHKeyExchange(with: theirEncryptionPublicKeyString)
+      } catch {
+        NSLog(".ECDH0 error caught:\n\(error)")
+        fallthrough
+      }
+
+      let msg = [
+        MessageType.ECDH1.rawValue,
+        ephemeralPublicKeyString,
+        signatureString,
+        signingPublicKeyString
+      ].joined(separator: "|")
+
+      controller.clearInputText()
+      controller.textDocumentProxy.insertText(msg)
+
+      // TODO: placeholder
+      textView.text = "Request to generate symmetric key received."
+
+    case .ECDH1:
+      // Reposne to request to ECDH. Expect to receive ephemeral public key.
+      // Verify signature, compute and save symmetric key.
+      // Expected format: "{.ECDH1}|{ephemeralPublicKey}|{signature}|{signingPublicKey}"
+      if tokens.count != 4 { fallthrough }
+
+      do {
+        try keys.verifyECDHKeyExchangeResponse(
+          ephemeralPublicKeyString: tokens[1],
+          signatureString: tokens[2],
+          theirSigningPublicKeyString: tokens[3]
+        )
+      } catch {
+        NSLog(".ECDH1 error caught:\n\(error)")
+        fallthrough
+      }
+
+
+      // TODO: placeholder
+      textView.text = "Symmetric key generated"
+
+    case .ciphertext:
+      // Ciphertext received. Verify signature and decrypt using symmetric key.
+      if tokens.count != 4 { fallthrough }
+      var plaintext: String
+
+      do {
+        plaintext = try keys.decrypt((tokens[1], tokens[2]), from: tokens[3])
+      } catch {
+          NSLog(".ciphertext error caught:\n\(error)")
+          fallthrough
+      }
+
+      // TODO: placeholder
+      textView.text = "Decrypted Message:\n\(plaintext)"
+
+    default:
+      textView.text = "Unknown type of message copied."
+    }
+
+
+  }
+
+  @objc func sealButtonPressed(_ sender: Any) {
+    // TODO: finish
+    let textInput = (controller.textDocumentProxy.documentContextBeforeInput ?? "") +
+      (controller.textDocumentProxy.selectedText ?? "") +
+      (controller.textDocumentProxy.documentContextAfterInput ?? "")
+
+    if textInput.isEmpty {
+      textView.text = "Unable to seal message because input text field is empty."
+      return
+    }
+
+    var ciphertextString, signatureString, signingPublicKeyString: String!
+
+    do {
+      (ciphertextString, signatureString, signingPublicKeyString) = try keys.encrypt(textInput)
+    } catch {
+      NSLog("encryptButtonPressed error caught:\n\(error)")
+      textView.text = "Something went wrong. Unable to encrypt. Try again later."
+      return
+    }
     let msg = [
-      MessageType.ECDH1.rawValue,
-      ephemeralPublicKeyString,
+      MessageType.ciphertext.rawValue,
+      ciphertextString,
       signatureString,
       signingPublicKeyString
     ].joined(separator: "|")
 
-    clearInputText()
-    textDocumentProxy.insertText(msg)
+    controller.clearInputText()
+    controller.textDocumentProxy.insertText(msg)
 
-    // TODO: placeholder
-    textBox.text = "Request to generate symmetric key received."
+    textView.text = "Text encrypted! Ready to be sent."
 
-  case .ECDH1:
-    // Reposne to request to ECDH. Expect to receive ephemeral public key.
-    // Verify signature, compute and save symmetric key.
-    // Expected format: "{.ECDH1}|{ephemeralPublicKey}|{signature}|{signingPublicKey}"
-    if tokens.count != 4 { fallthrough }
-
-    do {
-      try keys.verifyECDHKeyExchangeResponse(
-        ephemeralPublicKeyString: tokens[1],
-        signatureString: tokens[2],
-        theirSigningPublicKeyString: tokens[3]
-      )
-    } catch {
-      NSLog(".ECDH1 error caught:\n\(error)")
-      fallthrough
-    }
-
-
-    // TODO: placeholder
-    textBox.text = "Symmetric key generated"
-
-  case .ciphertext:
-    // Ciphertext received. Verify signature and decrypt using symmetric key.
-    if tokens.count != 4 { fallthrough }
-    var plaintext: String
-
-    do {
-      plaintext = try keys.decrypt((tokens[1], tokens[2]), from: tokens[3])
-    } catch {
-        NSLog(".ciphertext error caught:\n\(error)")
-        fallthrough
-    }
-
-    // TODO: placeholder
-    textBox.text = "Decrypted Message:\n\(plaintext)"
-
-  default:
-    textBox.text = "Unknown type of message copied."
   }
 
-
-}
-
-func sealButtonPressed(_ sender: Any) {
-  // TODO: finish
-  let textInput = (textDocumentProxy.documentContextBeforeInput ?? "") +
-    (textDocumentProxy.selectedText ?? "") +
-    (textDocumentProxy.documentContextAfterInput ?? "")
-
-  if textInput.isEmpty {
-    textBox.text = "Unable to seal message because input text field is empty."
-    return
-  }
-
-  var ciphertextString, signatureString, signingPublicKeyString: String!
-
-  do {
-    (ciphertextString, signatureString, signingPublicKeyString) = try keys.encrypt(textInput)
-  } catch {
-    NSLog("encryptButtonPressed error caught:\n\(error)")
-    textBox.text = "Something went wrong. Unable to encrypt. Try again later."
-    return
-  }
-
-
-  let msg = [
-    MessageType.ciphertext.rawValue,
-    ciphertextString,
-    signatureString,
-    signingPublicKeyString
-  ].joined(separator: "|")
-
-  clearInputText()
-  textDocumentProxy.insertText(msg)
-
-  textBox.text = "Text encrypted! Ready to be sent."
-
-}
-*/
 }
